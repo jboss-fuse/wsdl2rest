1e235c47d00d1922a24634fb9db90271545e6473
Thomas Diesler <tdiesler@redhat.com>
2018-01-12 14:44:17 +0100

    [related #19] Generate client proxy instead of using provided bean implementation
    Currently blocked by #37

--- jaxws/src/test/java/org/jboss/fuse/wsdl2rest/test/rpclit/CamelCxfWsRpcLitTest.java
+++ jaxws/src/test/java/org/jboss/fuse/wsdl2rest/test/rpclit/CamelCxfWsRpcLitTest.java
@@ -22,6 +22,10 @@ import java.net.URL;
 import java.util.Date;
 import java.util.List;
 
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.MediaType;
 import javax.xml.namespace.QName;
 import javax.xml.ws.Service;
 
@@ -33,11 +37,15 @@ import org.jboss.fuse.wsdl2rest.jaxws.ItemBuilder;
 import org.jboss.fuse.wsdl2rest.jaxws.rpclit.Address;
 import org.jboss.fuse.wsdl2rest.util.SpringCamelContextFactory;
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
 
 public class CamelCxfWsRpcLitTest {
 
-    static String CONTEXT_URL = "http://0.0.0.0:8081/rs";
+    static String CONTEXT_URL = "http://0.0.0.0:8081/jaxrs";
     
     @Test
     public void testJavaClient() throws Exception {
@@ -86,6 +94,60 @@ public class CamelCxfWsRpcLitTest {
             Assert.assertEquals("Kermit", producer.requestBody("direct:getAddress", 100, Item.class).getName());
             Assert.assertEquals(100, (int) producer.requestBody("direct:updAddress", frog, Integer.class));
             Assert.assertEquals("Frog", producer.requestBody("direct:getAddress", 100, Item.class).getName());
+        } finally {
+            camelctx.stop();
+        }
+    }
+
+    @Test
+    @Ignore("[#37] JAXRS message cannot be routed to JAXWS proxy") 
+    public void testRestClient() throws Exception {
+
+        URL resourceUrl = getClass().getResource("/rpclit/rpclit-camel-context.xml");
+        CamelContext camelctx = SpringCamelContextFactory.createSingleCamelContext(resourceUrl, null);
+        camelctx.start();
+        try {
+            Assert.assertEquals(ServiceStatus.Started, camelctx.getStatus());
+
+            Client client = ClientBuilder.newClient().register(JacksonJsonProvider.class);
+            
+            Date dob = Item.DATE_FORMAT.parse("11.11.1968");
+            Item kermit = new ItemBuilder().id(100).name("Kermit").dateOfBirth(dob).build();
+            Item frog = new ItemBuilder().id(100).name("Frog").dateOfBirth(dob).build();
+            
+            // GET @Address#listAddresses()
+            String res1 = client.target(CONTEXT_URL + "/addresses").request().get(String.class);
+            Assert.assertEquals("", res1);
+
+            // POST @Address#addAddress(Item)
+            String payload = new ObjectMapper().writeValueAsString(kermit);
+            Integer res2 = client.target(CONTEXT_URL + "/address").request().post(Entity.entity(payload, MediaType.APPLICATION_JSON), Integer.class);
+            Assert.assertEquals(new Integer(100), res2);
+
+            // GET @Address#listAddresses()
+            List<?> res3 = client.target(CONTEXT_URL + "/addresses").request().get(List.class);
+            Assert.assertEquals("[100]", res3.toString());
+
+            // GET @Address#getAddress(int)
+            Item res4 = client.target(CONTEXT_URL + "/address/100").request().get(Item.class);
+            Assert.assertEquals("Kermit", res4.getName());
+
+            // PUT @Address#updAddress(Item)
+            payload = new ObjectMapper().writeValueAsString(frog);
+            Integer res5 = client.target(CONTEXT_URL + "/address").request().put(Entity.entity(payload, MediaType.APPLICATION_JSON), Integer.class);
+            Assert.assertEquals(new Integer(100), res5);
+
+            // GET @Address#getAddress(int)
+            Item res6 = client.target(CONTEXT_URL + "/address/100").request().get(Item.class);
+            Assert.assertEquals("Frog", res6.getName());
+
+            // DEL @Address#delAddress(int)
+            client.target(CONTEXT_URL + "/address/100").request().delete();
+            
+            // GET @Address#listAddresses()
+            String res7 = client.target(CONTEXT_URL + "/addresses").request().get(String.class);
+            Assert.assertEquals("[]", res7);
+
         } finally {
             camelctx.stop();
         }
